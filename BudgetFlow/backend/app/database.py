da2b7aa93a14b2f404571504a14a6ece858ee47a
Ashton <97850498+Ashtonmchenry@users.noreply.github.com>
2026-06-30 19:56:42 -0400

    adjusted file description for database.py

--- BudgetFlow/backend/app/database.py
+++ BudgetFlow/backend/app/database.py
@@ -1,10 +1,9 @@
-# Connects FastAPI to the SQLite database using SQLAlchemy.
+# This file sets up SQLAlchemy and the database session system, connecting FastAPI to the SQLite database
 # Defines the database URL, creates an engine, and sets up a session factory.
 # Also defines a base class for models and a dependency function to get a database session for API endpoints.
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
 
 from app.config import DATABASE_URL
 
